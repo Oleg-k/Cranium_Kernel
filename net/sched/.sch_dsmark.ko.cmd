31640c97ef7c21761e5904b1c7609b9acf863c14
Sarthak Acharya <sa6915@gmail.com>
2011-12-09 17:57:31 +0530

    net sched: fixed some kernel mem leaks

---
+++ net/sched/.sch_dsmark.ko.cmd
@@ -0,0 +1,1 @@
+cmd_net/sched/sch_dsmark.ko := /home/sarthak/Downloads/Toolchains/arm-eabi-4.4.3/bin/arm-eabi-ld -EL -r  -T /home/sarthak/Downloads/i9001/Cranium_Kernel/scripts/module-common.lds --build-id -o net/sched/sch_dsmark.ko net/sched/sch_dsmark.o net/sched/sch_dsmark.mod.o
