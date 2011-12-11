30040954a4ec7bca915b817e0f0813cb164918aa
Sarthak Acharya <sa6915@gmail.com>
2011-12-11 17:36:36 +0530

    NFS: Fix "kernel BUG at fs/aio.c:554!"

--- fs/nfs/direct.c
+++ fs/nfs/direct.c
@@ -400,21 +400,19 @@ static ssize_t nfs_direct_read_schedule_iovec(struct nfs_direct_req *dreq,
 		if ((size_t)result < vec->iov_len)
 			break;
 		pos += vec->iov_len;
-	}
-
-        /*
-         * If no bytes were started, return the error, and let the
-         * generic layer handle the completion.
-         */
-        if (requested_bytes == 0) {
-          nfs_direct_req_release(dreq);
-          return result < 0 ? result : -EIO;
-        }
-
+              }
+             /*
+              * If no bytes were started, return the error, and let the
+              * generic layer handle the completion.
+              */
+             if (requested_bytes == 0) {
+               nfs_direct_req_release(dreq);
+               return result < 0 ? result : -EIO;
+  	}
 
 	if (put_dreq(dreq))
 		nfs_direct_complete(dreq);
-        return 0;
+            return 0;
 }
 
 static ssize_t nfs_direct_read(struct kiocb *iocb, const struct iovec *iov,
@@ -832,20 +830,20 @@ static ssize_t nfs_direct_write_schedule_iovec(struct nfs_direct_req *dreq,
 		if ((size_t)result < vec->iov_len)
 			break;
 		pos += vec->iov_len;
-   }
-
-   /*
-    * If no bytes were started, return the error, and let the
-    * generic layer handle the completion.
-    */
-   if (requested_bytes == 0) {
-     nfs_direct_req_release(dreq);
-     return result < 0 ? result : -EIO;
 	}
+
+        /*
+         * If no bytes were started, return the error, and let the
+         * generic layer handle the completion.
+         */
+        if (requested_bytes == 0) {
+          nfs_direct_req_release(dreq);
+          return result < 0 ? result : -EIO;
+       }
 
 	if (put_dreq(dreq))
 		nfs_direct_write_complete(dreq, dreq->inode);
-	return 0;
+        return 0;
 }
 
 static ssize_t nfs_direct_write(struct kiocb *iocb, const struct iovec *iov,
