dd32fe0da658317be419a6303b98ddc9e45fb99c
Sarthak Acharya <sa6915@gmail.com>
2011-12-11 14:41:05 +0530

    next_pidmap: fix overflow condition

---
+++ include/generated/compile.h
@@ -0,0 +1,9 @@
+/* This file is auto generated, version 5 */
+/*  PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#5 PREEMPT Sun Dec 11 13:18:23 IST 2011"
+#define LINUX_COMPILE_TIME "13:18:23"
+#define LINUX_COMPILE_BY "root"
+#define LINUX_COMPILE_HOST "sarthak"
+#define LINUX_COMPILE_DOMAIN "(none)"
+#define LINUX_COMPILER "gcc version 4.4.3 (GCC) "
