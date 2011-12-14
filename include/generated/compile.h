d2ded09355f2e587e0cfeb0b9887e128faa5d3ee
Sarthak Acharya <sa6915@gmail.com>
2011-12-14 17:54:03 +0530

    PM / Runtime: Fix pm_runtime_suspended()

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,8 +1,8 @@
 /* This file is auto generated, version 5 */
 /*  PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#5 PREEMPT Tue Dec 13 16:01:45 IST 2011"
-#define LINUX_COMPILE_TIME "16:01:45"
+#define UTS_VERSION "#5 PREEMPT Wed Dec 14 17:45:33 IST 2011"
+#define LINUX_COMPILE_TIME "17:45:33"
 #define LINUX_COMPILE_BY "root"
 #define LINUX_COMPILE_HOST "sarthak"
 #define LINUX_COMPILE_DOMAIN "(none)"
