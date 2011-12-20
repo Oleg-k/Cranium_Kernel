11dac6fd373918ae9db5c3093c68a6425a519294
Sarthak Acharya <sa6915@gmail.com>
2011-12-20 14:26:16 +0530

    netlink: Make nlmsg_find_attr take a const nlmsghdr*;scripts/kallsyms: Enable error messages while hush up unnecessary war

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,8 +1,8 @@
-/* This file is auto generated, version 5 */
+/* This file is auto generated, version 6 */
 /*  PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#5 PREEMPT Wed Dec 14 17:45:33 IST 2011"
-#define LINUX_COMPILE_TIME "17:45:33"
+#define UTS_VERSION "#6 PREEMPT Tue Dec 20 12:51:26 IST 2011"
+#define LINUX_COMPILE_TIME "12:51:26"
 #define LINUX_COMPILE_BY "root"
 #define LINUX_COMPILE_HOST "sarthak"
 #define LINUX_COMPILE_DOMAIN "(none)"
--- include/generated/utsrelease.h
+++ include/generated/utsrelease.h
@@ -1,1 +1,1 @@
-#define UTS_RELEASE "2.6.35.7-Cranium-OC/UV-5.2"
+#define UTS_RELEASE "2.6.35.7-Cranium-OC/UV"
