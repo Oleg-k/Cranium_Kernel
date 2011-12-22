f142f5c767662cd38e7f38db96e979a584aa81fd
Sarthak Acharya <sa6915@gmail.com>
2011-12-22 17:22:33 +0530

    Overclock @1.9 Ghz;increased VM max readahead to 512

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,8 +1,8 @@
 /* This file is auto generated, version 6 */
 /*  PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#6 PREEMPT Thu Dec 22 13:22:34 IST 2011"
-#define LINUX_COMPILE_TIME "13:22:34"
+#define UTS_VERSION "#6 PREEMPT Thu Dec 22 14:29:22 IST 2011"
+#define LINUX_COMPILE_TIME "14:29:22"
 #define LINUX_COMPILE_BY "root"
 #define LINUX_COMPILE_HOST "sarthak"
 #define LINUX_COMPILE_DOMAIN "(none)"
