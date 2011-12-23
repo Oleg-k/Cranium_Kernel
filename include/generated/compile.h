be1b36cfd474d8c4b0236835e056ba6c2cdba84e
Sarthak Acharya <sa6915@gmail.com>
2011-12-23 08:11:20 +0530

    Fix Build(Kali-)

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,8 +1,8 @@
 /* This file is auto generated, version 6 */
 /*  PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#6 PREEMPT Thu Dec 22 14:29:22 IST 2011"
-#define LINUX_COMPILE_TIME "14:29:22"
+#define UTS_VERSION "#6 PREEMPT Fri Dec 23 08:03:40 IST 2011"
+#define LINUX_COMPILE_TIME "08:03:40"
 #define LINUX_COMPILE_BY "root"
 #define LINUX_COMPILE_HOST "sarthak"
 #define LINUX_COMPILE_DOMAIN "(none)"
