a87835747efe953500118731e3f94915548217e1
Sarthak Acharya <sa6915@gmail.com>
2011-12-31 10:26:16 +0530

    Added 2 Governors:Lazy & Lionheart,Fixed Build,Restrict Overlclock to 1,9 Ghz for stabilty-Cranium RC7 Changes

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,8 +1,8 @@
-/* This file is auto generated, version 6 */
+/* This file is auto generated, version 7 */
 /*  PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#6 PREEMPT Fri Dec 23 08:03:40 IST 2011"
-#define LINUX_COMPILE_TIME "08:03:40"
+#define UTS_VERSION "#7 PREEMPT Sat Dec 31 10:17:42 IST 2011"
+#define LINUX_COMPILE_TIME "10:17:42"
 #define LINUX_COMPILE_BY "root"
 #define LINUX_COMPILE_HOST "sarthak"
 #define LINUX_COMPILE_DOMAIN "(none)"
