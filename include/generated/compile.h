0f858ffa6b1e05df8de5914af9718cfb07ec837c
Sarthak Acharya <sa6915@gmail.com>
2012-03-05 15:56:50 +0530

    Make source buildable

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,9 +1,9 @@
 /* This file is auto generated, version 8 */
 /*  PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#8 PREEMPT Fri Jan 6 17:12:50 IST 2012"
-#define LINUX_COMPILE_TIME "17:12:50"
+#define UTS_VERSION "#8 PREEMPT Mon Mar 5 15:54:42 IST 2012"
+#define LINUX_COMPILE_TIME "15:54:42"
 #define LINUX_COMPILE_BY "root"
 #define LINUX_COMPILE_HOST "sarthak"
 #define LINUX_COMPILE_DOMAIN "(none)"
-#define LINUX_COMPILER "gcc version 4.4.3 (GCC) "
+#define LINUX_COMPILER "gcc version 4.5.2 (Sourcery G++ Lite 2011.03-41) "
